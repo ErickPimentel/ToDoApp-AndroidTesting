d449d15b63523158318f8200fc9f3ac952c165db
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-05-18 16:36:59 -0300

    writingg your first test

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,21 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest{
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero(){
+        val tasks = listOf<Task>(
+            Task("title", "desc", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(result.completedTasksPercent, `is` (0f))
+        assertEquals(result.activeTasksPercent, `is` (100f))
+    }
+}
